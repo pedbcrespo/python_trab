dc5e3c1978ba7d8d2e9724cf990f59b4a72263ff
Pedro <pedbcrespo@gmail.com>
2020-07-21 13:47:51 -0300

    commit on master

--- func_dados.py
+++ func_dados.py
@@ -1,40 +1,46 @@
 import json
 
-def carregar(nome):
+def carregar(arquivo):
+    with open(arquivo,'r') as json_file:
+        try:
+            dados = json.load(json_file)
+            return dados
+        except:
+            print('ERRO')    
+            return False
+            
+def carregar_campo(camp):
     '''
     :return: uma lista de dicionarios do arquivo .json
     '''
-    url = '{}.json'.format(nome)
     lista = []
-    with open(url,'r') as json_file:
-        try:
-            dados = json.load(json_file)
-            for dado in dados["jogos"]:
-                lista.append(dado)
-        except:
-            print("ERRO - dados_{} nao encontrados".format(nome))
+    with open('dados.json','r') as json_file:
+        dados = carregar('dados.json')
+        for dado in dados[camp]:
+            lista.append(dado)
     return lista
 
 def carregar_usuarios():
-    return carregar("usuarios")
+    return carregar_campo("usuarios")
 def carregar_jogos():
-    return carregar("jogos")
+    return carregar_campo("jogos")
 
-def salvar(elem):
-    '''
-        :param lista: uma lista de dicionarios, ou seja, {'nome_do_campo': variavel}
-        :return: True se der certo, False se der errado
-    '''
-    def _slv(lista):
-        dic = {elem: lista}
-        with open('jogos.json', 'w') as json_file:
+def salvar(camp):
+    def slv(lista):
+        dados = carregar('dados.json')
+        dados[camp] = lista
+        
+        with open('dados.json','w') as json_file:
             try:
-                json.dump(dic, json_file, indent=4)
+                json.dump(dados, json_file, indent=4)
             except:
-                print('ERRO')
-                return False
-            return True
-    return _slv
+                print("ERRO ao salvar")
 
+    return slv
+                
 salvar_usuarios = salvar("usuario")
 salvar_jogos = salvar("jogos")
+
+
+if __name__ == '__main__':
+    print(carregar('dados.json')['usuarios'])
